223443d1e0d40beb7c44b698c1e275b649baf039
CezarPetreanu <cezarpetreanu4013@gmail.com>
2022-12-06 23:55:27 +0200

    preview complete

--- src/StivaMini/PreviewArea.java
+++ src/StivaMini/PreviewArea.java
@@ -1,11 +1,13 @@
 package StivaMini;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +37,7 @@ public class PreviewArea extends JComponent{
 	}
 	
 	public void clear() {
+		/*
 		g2.setPaint(Color.white);
 		for(int i=0; i<12; i++)
 			for(int j=0; j<12; j++) {
@@ -44,23 +47,25 @@ public class PreviewArea extends JComponent{
 					g2.setPaint(Color.gray);
 				g2.fillRect(i*32, j*32, 32, 32);
 			}
+		*/
 		for(int i=0; i<numberOfLayers; i++) {
 			Image imageLayer = paintLayer(i);
 			
 			AffineTransform identity = new AffineTransform();
 			AffineTransform trans = new AffineTransform();
 			trans.setTransform(identity);
-			trans.rotate( Math.toRadians(30) );
-			
+			trans.scale(1, 1);
+			trans.translate(80, 80-i*8);
+			trans.rotate(Math.toRadians(30), imageLayer.getWidth(null)/2, imageLayer.getHeight(null)/2);
 			g2.drawImage(imageLayer, trans, null);
 			//g2.drawImage(imageLayer, 80, 80-i*4, null);
+			repaint();
 		}
 	}
 	
-	public Image paintLayer(int layerNumber) {
-		Image imageLayer;
+	public BufferedImage paintLayer(int layerNumber) {
+		BufferedImage imageLayer = new BufferedImage(160, 160, BufferedImage.TYPE_INT_ARGB);;
 		Graphics2D g2Layer;
-		imageLayer = createImage(160, 160);
 		g2Layer = (Graphics2D) imageLayer.getGraphics();
 		g2Layer.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
 
